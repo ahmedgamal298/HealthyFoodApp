e33e16f54fd8075d8a3c1a9a578951c5555c0c48
Ahmed Gamal <ahmedgamal12346@gmail.com>
2019-12-31 22:33:52 +0200

    Post Image

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:healthy_food/PhotoUpload.dart';
 import 'Authentication.dart';
+import 'Posts.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({
@@ -11,65 +15,143 @@ class HomePage extends StatefulWidget {
   final AuthImplementation auth;
   final VoidCallback onSignedOut;
 
-  @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Posts> postsList = [];
+
+  @override
+  void iniState() {
+    super.initState();
+    DatabaseReference postRef =
+        FirebaseDatabase.instance.reference().child("Posts");
+    postRef.once().then((DataSnapshot snap) {
+      var KEYS = snap.value.keys;
+      var DATA = snap.value;
+      postsList.clear();
+      for (var individualKey in KEYS) {
+        Posts posts = Posts(
+            DATA[individualKey]['image'],
+            DATA[individualKey]['description'],
+            DATA[individualKey]['date'],
+            DATA[individualKey]['time']);
+        postsList.add(posts);
+      }
+      setState(() {
+        print('length: $postsList.lenght');
+      });
+    });
+  }
 
   //Methods
   void _logoutUser() async {
-    try{
+    try {
       await widget.auth.signOut();
       widget.onSignedOut();
+    } catch (e) {
+      print(" Error = " + e.toString());
     }
-    catch(e){
-      print( " Error = " + e.toString());
-    }
-
   }
 
   //design
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('News Feed'),
       ),
       body: Container(
-
+        child: postsList.length == 0
+            ? Text(
+                "No Posts Available ",
+                style: TextStyle(fontSize: 16),
+              )
+            : ListView.builder(
+                itemCount: postsList.length,
+                itemBuilder: (_HomePageState_, index) {
+                  return PostUI(
+                      postsList[index].image,
+                      postsList[index].description,
+                      postsList[index].date,
+                      postsList[index].time);
+                },
+              ),
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.pink,
         child: Container(
-          margin: const EdgeInsets.only(left: 70.0 , right: 70.0),
-          child:  Row(
+          margin: const EdgeInsets.only(left: 70.0, right: 70.0),
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               IconButton(
-                icon: Icon(Icons.local_car_wash),
-                iconSize: 50,
-                color: Colors.white,
-                tooltip: "Nice",
-                onPressed : _logoutUser,
+                icon: Text(
+                  "Log out",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                iconSize: 60,
+                tooltip: "logout",
+                onPressed: _logoutUser,
               ),
               IconButton(
                 icon: Icon(Icons.add_a_photo),
-                iconSize: 50.0,
-                color: Colors.pinkAccent,
-                onPressed: (){
-                  Navigator.push
-                    (context,
-                      MaterialPageRoute(builder: (context){
-                        return UploadPhotoPage();
-                      })
-                  );
+                iconSize: 40.0,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return UploadPhotoPage();
+                  }));
                 },
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget PostUI(String image, String description, String date, String time) {
+    return Card(
+      elevation: 10.0,
+      margin: EdgeInsets.all(15.0),
+      child: Container(
+        padding: EdgeInsets.all(14.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  date,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  time,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Image.network(
+              image,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              description,
+              style: Theme.of(context).textTheme.subhead,
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
